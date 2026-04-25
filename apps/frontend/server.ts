d51bf69cf6476c3d53cb3879a4e6182a00d55f6d
iRogulj <ivan.rogulj92@gmail.com>
2026-04-25 19:00:43 +0200

    fix

--- apps/frontend/server.ts
+++ apps/frontend/server.ts
@@ -1,31 +1,39 @@
-import { AngularNodeAppEngine, writeResponseToNodeResponse } from '@angular/ssr/node';
+import 'zone.js/node';
+
+import { APP_BASE_HREF } from '@angular/common';
+import { CommonEngine } from '@angular/ssr/node';
 import * as express from 'express';
+import { existsSync } from 'node:fs';
 import { join } from 'node:path';
+import bootstrap from './src/main.server';
 
 export function app(): express.Express {
   const server = express();
-  const browserDistFolder = join(__dirname, '../browser');
-  const angularApp = new AngularNodeAppEngine();
+  const distFolder = join(__dirname, '../browser');
+  const indexHtml = existsSync(join(distFolder, 'index.original.html'))
+    ? join(distFolder, 'index.original.html')
+    : join(distFolder, 'index.html');
 
-  server.use(
-    express.static(browserDistFolder, { maxAge: '1y', index: false, redirect: false }),
-  );
+  const commonEngine = new CommonEngine();
 
-  server.use(
-    '/**',
-    (req: express.Request, res: express.Response, next: express.NextFunction) => {
-      angularApp
-        .handle(req)
-        .then(response => {
-          if (response) {
-            writeResponseToNodeResponse(response, res);
-          } else {
-            next();
-          }
-        })
-        .catch(next);
-    },
-  );
+  server.set('view engine', 'html');
+  server.set('views', distFolder);
+
+  server.get('*.*', express.static(distFolder, { maxAge: '1y' }));
+
+  server.get('*', (req, res, next) => {
+    const { protocol, originalUrl, baseUrl, headers } = req;
+    commonEngine
+      .render({
+        bootstrap,
+        documentFilePath: indexHtml,
+        url: `${protocol}://${headers.host}${originalUrl}`,
+        publicPath: distFolder,
+        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
+      })
+      .then((html: string) => res.send(html))
+      .catch((err: Error) => next(err));
+  });
 
   return server;
 }
